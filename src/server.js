cdc7d674e42a8da5ecc1c6b33557b5b91bf85aed
Shubham singh <shubhamshubhank1997@gmail.com>
2024-05-24 11:00:07 +0530

    .env file added

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "src/.env" });
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -7,17 +8,17 @@ app.use(express.json());
 //routes
 const listRoutes = require("./routes/list.routes");
 
-//mongoose connection ;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/myList";
+// // const MONGODB_URI = "mongodb://127.0.0.1:27017/myList";
+
 mongoose
-  .connect(MONGODB_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("connected to database ☀ ✔"))
   .catch((err) => console.log("facing error in database connection", err));
 
 app.get("/", (req, res) => res.send("hello ! server is working fine"));
 app.use("/api/list", listRoutes);
 
-const port = 5000;
+const port = process.env.port || 5000;
 app.listen(port, () => {
   console.log("app is listening on port ", port);
 });
